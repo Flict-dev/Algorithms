1ada222d50f3a1e2507a5cd13dfafe7e27e48feb
Flict-dev <zhdanom.maksi@gmail.com>
2023-02-28 21:42:44 +0300

    alg trainings 3.0

--- binary_search/tests/test_.py
+++
@@ -1,5 +0,0 @@
-from binary_search.main import binary_search
-
-
-def test_bin_search():
-    assert binary_search([i for i in range(10000)], 9998) != -1
---
+++ infix&postfix/main.py
@@ -0,0 +1,46 @@
+operations = {
+    "+": 1,
+    "-": 1,
+    "*": 2,
+    "/": 2,
+}
+
+
+def infix_to_postfix(row: str):
+    res, stack = "", []
+
+    for i in row:
+
+        if i.isdigit():
+            res += i
+
+        elif len(stack) == 0:
+            stack.append(i)
+
+        elif i == "(":
+            stack.append(i)
+
+        elif stack[-1] == "(":
+            stack.append(i)
+
+        elif i == ")":
+            for j in range(len(stack) - 1, 0, -1):
+                if stack[j] != "(":
+                    res += stack.pop()
+                else:
+                    stack.pop()
+                    break
+
+        elif operations[i] <= operations[stack[-1]]:
+            while operations[i] <= operations[stack[-1]]:
+                res += stack.pop()
+            stack.append(i)
+
+        else:
+            stack.append(i)
+
+    return res + "".join(stack)
+
+print("6+3*(1+4*5)*2")
+print(infix_to_postfix("6+3*(1+4*5)*2"))
+
